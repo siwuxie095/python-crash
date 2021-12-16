0e445f0d8dd9888869afd7ccf350023930ff8e28
siwuxie095 <834879583@qq.com>
2021-12-16 22:44:23 +0800

    Python 之禅；

---
+++ siwuxie095/python_crash/chapter_02/example_05/main.py
@@ -0,0 +1,76 @@
+"""
+
+Python 之禅
+
+
+经验丰富的程序员倡导尽可能避繁就简。Python 社区的理念都包含在 Tim Peters 撰写的 “Python 之禅” 中。要获悉这些
+有关编写优秀 Python 代码的指导原则，只需在解释器中执行命令 import this。这里不打算赘述整个 “Python之禅”，而
+只与大家分享其中的几条原则，让你明白为何它们对 Python 新手来说至关重要。
+
+>>> import this
+The Zen of Python, by Tim Peters
+Beautiful is better than ugly.
+
+Python 程序员笃信代码可以编写得漂亮而优雅。编程是要解决问题的，设计良好、高效而漂亮的解决方案都会让程序员心生敬意。
+随着你对 Python 的认识越来越深入，并使用它来编写越来越多的代码，有一天也许会有人站在你后面惊呼：“哇，代码编写得真
+是漂亮！”
+
+
+Simple is better than complex.
+
+如果有两个解决方案，一个简单、一个复杂，但都行之有效，就选择简单的解决方案吧。这样，你编写的代码将更容易维护，你或
+他人以后改进这些代码时也会更容易。
+
+
+Complex is better than complicated.
+
+现实是复杂的，有时候可能没有简单的解决方案。在这种情况下，就选择最简单可行的解决方案吧。
+
+
+Readability counts.
+
+即便是复杂的代码，也要让它易于理解。开发的项目涉及复杂代码时，一定要为这些代码编写有益的注释。
+
+
+There should be one-- and preferably only one --obvious way to do it.
+
+如果让两名 Python 程序员去解决同一个问题，他们提供的解决方案应大致相同。这并不是说编程没有创意空间，而是恰恰相反！
+然而，大部分编程工作是使用常见解决方案来解决简单的小问题，但这些小问题都包含在更庞大、更有创意空间的项目中。在你的
+程序中，各种具体细节对其他 Python 程序员来说都应易于理解。
+
+
+Now is better than never.
+
+你可以用余生来学习 Python 和编程的纷繁难懂之处，但这样你什么项目都完不成。不要企图编写完美无缺的代码，而是要先编写
+行之有效的代码，再决定是对其做进一步改进，还是转而去编写新代码。
+
+
+等你开始研究更复杂的主题时，务必牢记这种简约而清晰的理念。如此，经验丰富的程序员定将对你编写的代码心生敬意，进而乐意
+向你提供反馈，并与你合作开发有趣的项目。
+
+
+"Python 之禅" 全文如下：
+
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+
+"""
