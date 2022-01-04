3f7edeeef4333d05d8fb47784e209d11497cbc8d
siwuxie095 <834879583@qq.com>
2022-01-04 22:44:08 +0800

    安装 Pygame；

---
+++ siwuxie095/python_crash/chapter_12/example_03/main.py
@@ -0,0 +1,22 @@
+"""
+
+安装 Pygame
+
+
+开始编码前，先来安装 Pygame。可使用 pip 模块来帮助下载并安装 Python 包。要安装 Pygame，在终端提示符下
+执行如下命令：
+
+$ python -m pip install --user pygame
+
+这个命令让 Python 运行 pip 模块，将 pygame 包添加到当前用户的 Python 安装中。如果你运行程序或启动终端
+会话时使用的命令不是 python，而是 python3，请执行如下命令来安装 Pygame：
+
+$ python3 -m pip install --user pygame
+
+注意：如果该命令在 macOS 系统中不管用，请尝试在不指定标志 --user 的情况下再次执行。
+
+安装完成会出现：
+
+Successfully installed pygame-2.1.2
+
+"""
