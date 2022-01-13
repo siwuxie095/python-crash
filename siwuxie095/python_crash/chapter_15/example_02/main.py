6afaab8e59359a525455634be72e12c4c4060a34
siwuxie095 <834879583@qq.com>
2022-01-13 23:18:41 +0800

    安装 Matplotlib；

---
+++ siwuxie095/python_crash/chapter_15/example_02/main.py
@@ -0,0 +1,21 @@
+"""
+
+安装 Matplotlib
+
+
+这里将首先使用 Matplotlib 来生成几个图表，为此需要使用 pip 来安装它。pip 是一个可用于下载并安装
+Python 包的模块。请在终端提示符下执行如下命令：
+
+python -m pip install --user matplotlib
+
+这个命令让 Python 运行模块 pip，并将 matplotlib 包添加到当前用户的 Python 安装中。在你的系统
+中，如果运行程序或启动终端会话时使用的命令不是 python，而是 python3，应使用类似于下面的命令：
+
+python3 -m pip install --user matplotlib
+
+注意：在 macOS 系统中，如果这样不管用，请尝试在不指定标志 --user 的情况下再次执行该命令。
+
+要查看使用 Matplotlib 可制作的各种图表，请访问其官方网站，浏览示例画廊。通过单击画廊中的图表，可
+查看生成它们的代码。
+
+"""
